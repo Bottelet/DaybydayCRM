000681e5c8270472e205eaff75a27efa7b246749
bottelet <cbottelet@gmail.com>
2016-08-26 21:58:57 +0200

    Changed queue requirements for 5.3

--- config/queue.php
+++ config/queue.php
@@ -36,33 +36,33 @@ return [
         ],
 
         'database' => [
-            'driver' => 'database',
-            'table'  => 'jobs',
-            'queue'  => 'default',
-            'expire' => 60,
+            'driver'        => 'database',
+            'table'         => 'jobs',
+            'queue'         => 'default',
+            'retry_after'   => 60,
         ],
 
         'beanstalkd' => [
-            'driver' => 'beanstalkd',
-            'host'   => 'localhost',
-            'queue'  => 'default',
-            'ttr'    => 60,
+            'driver'         => 'beanstalkd',
+            'host'           => 'localhost',
+            'queue'          => 'default',
+            'retry_after'    => 60,
         ],
 
         'sqs' => [
-            'driver' => 'sqs',
-            'key'    => 'your-public-key',
-            'secret' => 'your-secret-key',
-            'prefix' => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
-            'queue'  => 'your-queue-name',
-            'region' => 'us-east-1',
+            'driver'         => 'sqs',
+            'key'            => 'your-public-key',
+            'secret'         => 'your-secret-key',
+            'prefix'         => 'https://sqs.us-east-1.amazonaws.com/your-account-id',
+            'queue'          => 'your-queue-name',
+            'region'         => 'us-east-1',
         ],
 
         'redis' => [
-            'driver'     => 'redis',
-            'connection' => 'default',
-            'queue'      => 'default',
-            'expire'     => 60,
+            'driver'          => 'redis',
+            'connection'      => 'default',
+            'queue'           => 'default',
+            'retry_after'     => 60,
         ],
 
     ],
