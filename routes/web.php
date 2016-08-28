222e4579f1ca0eb20523e8461dc0f548768b95b3
bottelet <cbottelet@gmail.com>
2016-08-28 21:56:52 +0200

    Update routes for laravel 5.3

--- routes/web.php
+++ routes/web.php
@@ -2,74 +2,91 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
 |
 */
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-});
-
-Route::group(['middleware' => ['web', 'auth']], function () {
+Route::auth();
+ 	/**
+     * MAIN
+     */
         Route::get('/', 'PagesController@dashboard');
+        Route::get('dashboard', 'PagesController@dashboard')->name('dashboard');
+    /**
+     * USERS
+     */
         Route::get('users/data', 'UsersController@anyData')->name('users.data');
         Route::get('users/taskdata/{id}', 'UsersController@taskData')->name('users.taskdata');
         Route::get('users/closedtaskdata/{id}', 'UsersController@closedTaskData')->name('users.closedtaskdata');
         Route::get('users/clientdata/{id}', 'UsersController@clientData')->name('users.clientdata');
-        Route::get('clients/data', 'ClientsController@anyData')->name('clients.data');
-        Route::get('tasks/data', 'TasksController@anyData')->name('tasks.data');
-        Route::get('leads/data', 'LeadsController@anyData')->name('leads.data');
         Route::resource('users', 'UsersController');
+        /* ROLES */
+        Route::resource('roles', 'RolesController');
+    /**
+     * CLIENTS
+     */
+        Route::get('clients/data', 'ClientsController@anyData')->name('clients.data');
         Route::post('clients/create/cvrapi', 'ClientsController@cvrapiStart');
         Route::post('clients/upload/{id}', 'DocumentsController@upload');
-        
         Route::resource('clients', 'ClientsController');
-        Route::get('settings', 'SettingsController@index')->name('settings.index');
-        Route::patch('settings/permissionsUpdate', 'SettingsController@permissionsUpdate');
-        Route::post('settings/stripe', 'SettingsController@stripe');
-        Route::patch('settings/overall', 'SettingsController@updateOverall');
+    /**
+     * TASKS
+     */
+        Route::get('tasks/data', 'TasksController@anyData')->name('tasks.data');
         Route::patch('tasks/updatestatus/{id}', 'TasksController@updateStatus');
         Route::patch('tasks/updateassign/{id}', 'TasksController@updateAssign');
         Route::post('tasks/updatetime/{id}', 'TasksController@updateTime');
         Route::post('tasks/invoice/{id}', 'TasksController@invoice');
-        Route::patch('leads/updateassign/{id}', 'LeadsController@updateAssign');
         Route::resource('tasks', 'TasksController');
-        Route::resource('leads', 'LeadsController');
         Route::post('tasks/comments/{id}', 'CommentController@store');
+    /**
+     * LEADS
+     */
+        Route::get('leads/data', 'LeadsController@anyData')->name('leads.data');
+        Route::patch('leads/updateassign/{id}', 'LeadsController@updateAssign');
+        Route::resource('leads', 'LeadsController');
         Route::post('leads/notes/{id}', 'NotesController@store');
         Route::patch('leads/updatestatus/{id}', 'LeadsController@updateStatus');
         Route::patch('leads/updatefollowup/{id}', 'LeadsController@updateFollowup')->name('leads.followup');
-        
+    /**
+     * SETTINGS
+     */
+        Route::get('settings', 'SettingsController@index')->name('settings.index');
+        Route::patch('settings/permissionsUpdate', 'SettingsController@permissionsUpdate');
+        Route::patch('settings/overall', 'SettingsController@updateOverall');
+    /**
+     * DEPARTMENTS
+     */
         Route::resource('departments', 'DepartmentsController');
-        Route::resource('roles', 'RolesController');
         
-        Route::get('dashboard', 'PagesController@dashboard')->name('dashboard');
+              
+    /**
+     * INTEGRATIONS
+     */
         Route::resource('integrations', 'IntegrationsController');
-        
-        //Notifications
+      
+        /* SLACK */
+        Route::get('Integration/slack', 'IntegrationsController@slack');
+    /**
+     * NOTIFICATIONS
+     */
         Route::get('notifications/getall', 'NotificationsController@getAll')->name('notifications.get');
         Route::post('notifications/markread', 'NotificationsController@markRead');
         Route::get('notifications/markall', 'NotificationsController@markAll');
+    /**
+     * INVOICES
+     */
         Route::resource('invoices', 'InvoicesController');
-        Route::get('documents/import', 'DocumentsController@import');
         Route::post('invoice/updatepayment/{id}', 'InvoicesController@updatePayment')->name('invoice.payment.date');
         Route::post('invoice/reopenpayment/{id}', 'InvoicesController@reopenPayment')->name('invoice.payment.reopen');
         Route::post('invoice/sentinvoice/{id}', 'InvoicesController@updateSentStatus')->name('invoice.sent');
         Route::post('invoice/reopensentinvoice/{id}', 'InvoicesController@updateSentReopen')->name('invoice.sent.reopen');
         Route::post('invoice/newitem/{id}', 'InvoicesController@newItem')->name('invoice.new.item');
-});
+    /**
+     * IMPORT AND EXPORT
+     */
+        Route::get('documents/import', 'DocumentsController@import');
