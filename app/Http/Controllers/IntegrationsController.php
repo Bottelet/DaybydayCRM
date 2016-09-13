584a31e2f8c26e311fac7a1e374ca79e72f3bc76
Jayson Martinez <Jheysoon@users.noreply.github.com>
2016-09-13 17:35:16 +0800

    PSR-2

--- app/Http/Controllers/IntegrationsController.php
+++ app/Http/Controllers/IntegrationsController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use App\Models\Integration;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class IntegrationsController extends Controller
 {
