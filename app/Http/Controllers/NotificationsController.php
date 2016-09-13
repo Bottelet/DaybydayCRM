7ee459cd5a80018d174b8f702d23aeec2e561108
Jayson Martinez <Jheysoon@users.noreply.github.com>
2016-09-13 17:40:23 +0800

    PSR-2

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -1,10 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
-use App\Models\User;
 use Notifynder;
+use App\Models\User;
+use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
