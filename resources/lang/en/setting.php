175329e1737257ba8343dbf0ac6f1f97e352de6b
bottelet <cbottelet@gmail.com>
2016-11-01 19:03:55 +0100

    Translation for settings page

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'headers' =>  [
+        'settings' => 'Settings',
+        'overall' => 'Overall settings',
+        'save_role' => 'Save settings for role',
+        'allowed' => 'Allowed',
+        'not_allowed' => 'Not Allowed',
+    	'save_overall' => 'Save Overall Settings',
+    ],
+
+    'overall' => [
+    	'task' => [
+    		'completion' => 'Task completion',
+    		'completion_allowed' => 'If <b>Allowed</b> only user who are assigned the task & the admin can complete the task. ',
+    		'completion_not_allowed' => 'If <b>Not allowed</b> anyone, can complete all tasks.',
+    		'assigned' => 'Task Assigned user',
+    		'assigned_allowed' => 'If <b>Allowed</b> only user who are assigned the task &amp; the admin can assign another user.',
+    		'assigned_not_allowed' => 'If <b>Not allowed</b> anyone, can assign another user.'
+    	],
+    	'lead' => [
+    		'completion' => 'Lead completion',
+    		'completion_allowed' => 'If <b>Allowed</b> only user who are assigned the task &amp; the admin can complete the Lead. ',
+    		'completion_not_allowed' => 'If <b>Not allowed</b> anyone, can complete all Leads.',
+    		'assigned' => 'Lead Assigned user',
+    		'assigned_allowed' => 'If <b>Allowed</b> only user who are assigned the lead &amp; the admin can assign another user.',
+    		'assigned_not_allowed' => 'If <b>Not allowed</b> anyone, can assign another user.',
+    	]
+
+    ],
+
+];
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('heading')
-<h1>Settings</h1>
+<h1>@lang('setting.headers.settings')</h1>
 @stop
 @section('content')
 
@@ -57,7 +57,7 @@
 
   </tr>
   </div>
-    <td>{!! Form::submit('Save Settings for Role', ['class' => 'btn btn-primary']) !!}</td>  
+    <td>{!! Form::submit(Lang::get('setting.headers.save_role'), ['class' => 'btn btn-primary']) !!}</td>  
   {!! Form::close() !!}
   @endforeach
 
@@ -73,7 +73,7 @@
 
   
 <div class="row">
-    <div class="col-lg-12"><div class="sidebarheader movedown"><p>Overall settings</p></div>
+    <div class="col-lg-12"><div class="sidebarheader movedown"><p>@lang('setting.headers.overall')</p></div>
 
      
      {!! Form::model($settings, [
@@ -85,53 +85,53 @@
      *                     Task complete       
      *********************************************************************-->
          <div class="panel panel-default movedown">
-          <div class="panel-heading">Task completion</div>
+          <div class="panel-heading">@lang('setting.overall.task.completion')</div>
           <div class="panel-body">
 
-            If <b>Allowed</b> only user who are assigned the task &amp; the admin can complete the task. <br />
-            If <b>Not allowed</b> anyone, can complete all tasks.
+            @lang('setting.overall.task.completion_allowed') <br />
+           @lang('setting.overall.task.completion_not_allowed')
           </div>
         </div>
-            {!! Form::select('task_complete_allowed', [1 => 'Allowed', 2 => 'Not Allowed'], $settings->task_complete_allowed, ['class' => 'form-control']) !!}
+            {!! Form::select('task_complete_allowed', [1 => Lang::get('setting.headers.allowed'), 2 => Lang::get('setting.headers.not_allowed')], $settings->task_complete_allowed, ['class' => 'form-control']) !!}
   <!-- *********************************************************************
      *                     Task assign       
      *********************************************************************-->
          <div class="panel panel-default movedown">
-          <div class="panel-heading">Task Assigned user</div>
+          <div class="panel-heading">@lang('setting.overall.task.assigned')</div>
           <div class="panel-body">
 
-            If <b>Allowed</b> only user who are assigned the task &amp; the admin can assign another user. <br />
-            If <b>Not allowed</b> anyone, can assign another user.
+           @lang('setting.overall.task.assigned_allowed') <br />
+            @lang('setting.overall.task.assigned_not_allowed')
           </div>
         </div>
-            {!! Form::select('task_assign_allowed', [1 => 'Allowed', 2 => 'Not Allowed'], $settings->task_assign_allowed, ['class' => 'form-control']) !!}
+            {!! Form::select('task_assign_allowed', [1 => Lang::get('setting.headers.allowed'), 2 => Lang::get('setting.headers.not_allowed')], $settings->task_assign_allowed, ['class' => 'form-control']) !!}
   <!-- *********************************************************************
      *                     Lead complete       
      *********************************************************************-->
 
          <div class="panel panel-default movedown">
-          <div class="panel-heading">Lead completion</div>
+          <div class="panel-heading">@lang('setting.overall.lead.completion')</div>
           <div class="panel-body">
 
-            If <b>Allowed</b> only user who are assigned the task &amp; the admin can complete the Lead. <br />
-            If <b>Not allowed</b> anyone, can complete all Leads.
+             @lang('setting.overall.lead.completion_allowed')<br />
+           @lang('setting.overall.lead.completion_not_allowed')
           </div>
         </div>
-            {!! Form::select('lead_complete_allowed', [1 => 'Allowed', 2 => 'Not Allowed'], $settings->lead_complete_allowed, ['class' => 'form-control']) !!}
+            {!! Form::select('lead_complete_allowed', [1 => Lang::get('setting.headers.allowed'), 2 => Lang::get('setting.headers.not_allowed')], $settings->lead_complete_allowed, ['class' => 'form-control']) !!}
   <!-- *********************************************************************
      *                     Lead assign       
      *********************************************************************-->
          <div class="panel panel-default movedown">
-          <div class="panel-heading">Lead Assigned user</div>
+          <div class="panel-heading">@lang('setting.overall.lead.assigned')</div>
           <div class="panel-body">
 
-            If <b>Allowed</b> only user who are assigned the lead &amp; the admin can assign another user. <br />
-            If <b>Not allowed</b> anyone, can assign another user.
+             @lang('setting.overall.lead.assigned_allowed')<br />
+           @lang('setting.overall.lead.assigned_not_allowed')
           </div>
         </div>
-         {!! Form::select('lead_assign_allowed', [1 => 'Allowed', 2 => 'Not Allowed'], $settings->lead_assign_allowed, ['class' => 'form-control']) !!}
+         {!! Form::select('lead_assign_allowed', [1 => Lang::get('setting.headers.allowed'), 2 => Lang::get('setting.headers.not_allowed')], $settings->lead_assign_allowed, ['class' => 'form-control']) !!}
          <br />
-{!! Form::submit('Save Overall Settings', ['class' => 'btn btn-primary']) !!}
+{!! Form::submit(Lang::get('setting.headers.save_overall'), ['class' => 'btn btn-primary']) !!}
            {!! Form::close() !!}
      </div>
 </div>
