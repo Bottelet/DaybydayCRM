faf28523b97355d40809d97817a59e822ce1ed07
bottelet <cbottelet@gmail.com>
2016-11-24 22:34:39 +0100

    Clean up routes with group prefix, and put resources at bottom

--- routes/web.php
+++ routes/web.php
@@ -13,84 +13,105 @@
 Route::auth();
 Route::get('/logout', 'Auth\LoginController@logout');
 Route::group(['middleware' => ['auth']], function () {
+    
     /**
-     * MAIN
+     * Main
      */
         Route::get('/', 'PagesController@dashboard');
         Route::get('dashboard', 'PagesController@dashboard')->name('dashboard');
+
     /**
-     * USERS
+     * Users
      */
-        Route::get('users/data', 'UsersController@anyData')->name('users.data');
-        Route::get('users/taskdata/{id}', 'UsersController@taskData')->name('users.taskdata');
-        Route::get('users/closedtaskdata/{id}', 'UsersController@closedTaskData')->name('users.closedtaskdata');
-        Route::get('users/clientdata/{id}', 'UsersController@clientData')->name('users.clientdata');
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/data', 'UsersController@anyData')->name('users.data');
+        Route::get('/taskdata/{id}', 'UsersController@taskData')->name('users.taskdata');
+        Route::get('/closedtaskdata/{id}', 'UsersController@closedTaskData')->name('users.closedtaskdata');
+        Route::get('/clientdata/{id}', 'UsersController@clientData')->name('users.clientdata');
+    });
         Route::resource('users', 'UsersController');
-        /* ROLES */
+
+    /**
+     * Roles
+     */
         Route::resource('roles', 'RolesController');
+
     /**
-     * CLIENTS
+     * Clients
      */
-        Route::get('clients/data', 'ClientsController@anyData')->name('clients.data');
-        Route::post('clients/create/cvrapi', 'ClientsController@cvrapiStart');
-        Route::post('clients/upload/{id}', 'DocumentsController@upload');
+    Route::group(['prefix' => 'clients'], function () {
+        Route::get('/data', 'ClientsController@anyData')->name('clients.data');
+        Route::post('/create/cvrapi', 'ClientsController@cvrapiStart');
+        Route::post('/upload/{id}', 'DocumentsController@upload');
+    });
         Route::resource('clients', 'ClientsController');
+
     /**
-     * TASKS
+     * Tasks
      */
-        Route::get('tasks/data', 'TasksController@anyData')->name('tasks.data');
-        Route::patch('tasks/updatestatus/{id}', 'TasksController@updateStatus');
-        Route::patch('tasks/updateassign/{id}', 'TasksController@updateAssign');
-        Route::post('tasks/updatetime/{id}', 'TasksController@updateTime');
-        Route::post('tasks/invoice/{id}', 'TasksController@invoice');
+    Route::group(['prefix' => 'tasks'], function () {
+        Route::get('/data', 'TasksController@anyData')->name('tasks.data');
+        Route::patch('/updatestatus/{id}', 'TasksController@updateStatus');
+        Route::patch('/updateassign/{id}', 'TasksController@updateAssign');
+        Route::post('/updatetime/{id}', 'TasksController@updateTime');
+        Route::post('/invoice/{id}', 'TasksController@invoice');
+        Route::post('/comments/{id}', 'CommentController@store');
+    });
         Route::resource('tasks', 'TasksController');
-        Route::post('tasks/comments/{id}', 'CommentController@store');
+
     /**
-     * LEADS
+     * Leads
      */
-        Route::get('leads/data', 'LeadsController@anyData')->name('leads.data');
-        Route::patch('leads/updateassign/{id}', 'LeadsController@updateAssign');
+    Route::group(['prefix' => 'leads'], function () {
+        Route::get('/data', 'LeadsController@anyData')->name('leads.data');
+        Route::patch('/updateassign/{id}', 'LeadsController@updateAssign');
+        Route::post('/notes/{id}', 'NotesController@store');
+        Route::patch('/updatestatus/{id}', 'LeadsController@updateStatus');
+        Route::patch('/updatefollowup/{id}', 'LeadsController@updateFollowup')->name('leads.followup');
+    });
         Route::resource('leads', 'LeadsController');
-        Route::post('leads/notes/{id}', 'NotesController@store');
-        Route::patch('leads/updatestatus/{id}', 'LeadsController@updateStatus');
-        Route::patch('leads/updatefollowup/{id}', 'LeadsController@updateFollowup')->name('leads.followup');
+
     /**
-     * SETTINGS
+     * Settings
      */
-        Route::get('settings', 'SettingsController@index')->name('settings.index');
-        Route::patch('settings/permissionsUpdate', 'SettingsController@permissionsUpdate');
-        Route::patch('settings/overall', 'SettingsController@updateOverall');
+    Route::group(['prefix' => 'settings'], function () {
+        Route::get('/', 'SettingsController@index')->name('settings.index');
+        Route::patch('/permissionsUpdate', 'SettingsController@permissionsUpdate');
+        Route::patch('/overall', 'SettingsController@updateOverall');
+    });
+
     /**
-     * DEPARTMENTS
+     * Departments
      */
-        Route::resource('departments', 'DepartmentsController');
-        
-              
+        Route::resource('departments', 'DepartmentsController'); 
+
     /**
-     * INTEGRATIONS
+     * Integrations
      */
-        Route::resource('integrations', 'IntegrationsController');
-      
-        /* SLACK */
+    Route::group(['prefix' => 'integrations'], function () {
         Route::get('Integration/slack', 'IntegrationsController@slack');
+    });
+        Route::resource('integrations', 'IntegrationsController');
+
     /**
-     * NOTIFICATIONS
+     * Notifications
      */
-        Route::get('notifications/getall', 'NotificationsController@getAll')->name('notifications.get');
-        Route::post('notifications/markread', 'NotificationsController@markRead');
-        Route::get('notifications/markall', 'NotificationsController@markAll');
-        Route::get('notification/{id}', 'NotificationsController@markRead');
+    Route::group(['prefix' => 'notifications'], function () {
+        Route::get('/getall', 'NotificationsController@getAll')->name('notifications.get');
+        Route::post('/markread', 'NotificationsController@markRead');
+        Route::get('/markall', 'NotificationsController@markAll');
+        Route::get('/{id}', 'NotificationsController@markRead');
+    });
+
     /**
-     * INVOICES
+     * Invoices
      */
+    Route::group(['prefix' => 'invoices'], function () {
+        Route::post('/updatepayment/{id}', 'InvoicesController@updatePayment')->name('invoice.payment.date');
+        Route::post('/reopenpayment/{id}', 'InvoicesController@reopenPayment')->name('invoice.payment.reopen');
+        Route::post('/sentinvoice/{id}', 'InvoicesController@updateSentStatus')->name('invoice.sent');
+        Route::post('/reopensentinvoice/{id}', 'InvoicesController@updateSentReopen')->name('invoice.sent.reopen');
+        Route::post('/newitem/{id}', 'InvoicesController@newItem')->name('invoice.new.item');
+    });
         Route::resource('invoices', 'InvoicesController');
-        Route::post('invoice/updatepayment/{id}', 'InvoicesController@updatePayment')->name('invoice.payment.date');
-        Route::post('invoice/reopenpayment/{id}', 'InvoicesController@reopenPayment')->name('invoice.payment.reopen');
-        Route::post('invoice/sentinvoice/{id}', 'InvoicesController@updateSentStatus')->name('invoice.sent');
-        Route::post('invoice/reopensentinvoice/{id}', 'InvoicesController@updateSentReopen')->name('invoice.sent.reopen');
-        Route::post('invoice/newitem/{id}', 'InvoicesController@newItem')->name('invoice.new.item');
-    /**
-     * IMPORT AND EXPORT
-     */
-        Route::get('documents/import', 'DocumentsController@import');
 });
