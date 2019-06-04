90e0d01d1055f360bd849f7032ecb5a719dd5fb6
Jason Huebel <jhuebel@gopherindustrial.com>
2019-06-04 16:23:29 -0500

    responsive layout for client.create/update forms added billing and shipping addresses to clients addresses now include address1, address2, state and country removed VAT lookup, since it was limited to Denmark (may make the a config option)

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -6,23 +6,24 @@
 
 @section('content')
 
-    <?php $data = Session::get('data'); ?>
-
-    {!! Form::open(['url' => '/clients/create/cvrapi']) !!}
-        <div class="form-group">
-            <div class="input-group">
-                {!! Form::text('vat', null, ['class' => 'form-control', 'placeholder' => 'Insert company VAT']) !!}
-                <div class="popoverOption input-group-addon"
-                     rel="popover"
-                     data-placement="left"
-                     data-html="true"
-                     data-original-title="<span>Only for DK, atm.</span>">?
+<!--    <div class="container-fluid">
+        <?php $data = Session::get('data'); ?>
+        {!! Form::open(['url' => '/clients/create/cvrapi']) !!}
+            <div class="form-group">
+                <div class="input-group">
+                    {!! Form::text('vat', null, ['class' => 'form-control', 'placeholder' => 'Insert company VAT']) !!}
+                    <div class="popoverOption input-group-addon"
+                         rel="popover"
+                         data-placement="left"
+                         data-html="true"
+                         data-original-title="<span>Only for DK, atm.</span>">?
+                    </div>
                 </div>
+                {!! Form::submit('Get client info', ['class' => 'btn btn-primary clientvat']) !!}
             </div>
-            {!! Form::submit('Get client info', ['class' => 'btn btn-primary clientvat']) !!}
-        </div>
 
-    {!!Form::close()!!}
+        {!!Form::close()!!}
+    </div> -->
 
     {!! Form::open(['route' => 'clients.store']) !!}
         @include('clients.form', ['submitButtonText' => __('Create New Client')])
