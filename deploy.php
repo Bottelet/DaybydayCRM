882973b398c31ad65331a349bf36050f5094551e
Albertto Castro <albertto.programmer@gmail.com>
2020-06-22 18:00:31 -0300

    repo

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'my_project');
 
 // Project repository
-set('repository', 'git@github.com:Bottelet/flarepoint-crm.git');
+set('repository', 'https://github.com/Bottelet/flarepoint-crm.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
