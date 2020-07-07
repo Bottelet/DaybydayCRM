e3afad96984ae7e74ec77e31cee85e9d0dc1f0c3
Casper Bottelet <cbottelet@gmail.com>
2020-07-07 20:16:07 +0200

    fix dummy table seeder being removed in merge

--- database/seeds/UsersDummyTableSeeder.php
+++ database/seeds/UsersDummyTableSeeder.php
@@ -14,9 +14,6 @@ class UsersDummyTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\User::class, 5)->create()->each(function ($c) {
-        });
-
         $createDep = new Department;
         $createDep->id = '2';
         $createDep->name = 'Nerds';
@@ -30,45 +27,7 @@ class UsersDummyTableSeeder extends Seeder
         $createDep->external_id = Uuid::uuid4();
         $createDep->save();
 
-
-
-        $newrole = new RoleUser;
-        $newrole->role_id = '2';
-        $newrole->user_id = '2';
-        $newrole->timestamps = false;
-        $newrole->save();
-        $newrole = new RoleUser;
-        $newrole->role_id = '3';
-        $newrole->user_id = '3';
-        $newrole->timestamps = false;
-        $newrole->save();
-        $newrole = new RoleUser;
-        $newrole->role_id = '4';
-        $newrole->user_id = '4';
-        $newrole->timestamps = false;
-        $newrole->save();
-        $newrole = new RoleUser;
-        $newrole->role_id = '4';
-        $newrole->user_id = '5';
-        $newrole->timestamps = false;
-        $newrole->save();
-        $newrole = new RoleUser;
-        $newrole->role_id = '4';
-        $newrole->user_id = '6';
-        $newrole->timestamps = false;
-        $newrole->save();
-
-        \DB::table('department_user')->insert([
-            ['department_id' => 1,
-            'user_id' => 2],
-            ['department_id' => 2,
-            'user_id' => 3],
-            ['department_id' => 3,
-            'user_id' => 4],
-            ['department_id' => 3,
-            'user_id' => 5],
-            ['department_id' => 2,
-            'user_id' => 6]
-        ]);
+        factory(App\Models\User::class, 5)->create()->each(function ($c) {
+        });
     }
 }
