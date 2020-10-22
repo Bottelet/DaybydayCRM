a69390a57eb53c4c2c200da69b12a3cd03ccef66
Kath Young <kyoung@arris.com.au>
2020-10-23 07:47:20 +1030

    Add implementations for local storage

--- app/Services/Storage/Local.php
+++ app/Services/Storage/Local.php
@@ -1,33 +1,38 @@
 <?php
 namespace App\Services\Storage;
 
+use App\Models\Document;
 use App\Repositories\FilesystemIntegration\FilesystemIntegration;
+use Illuminate\Support\Facades\Storage;
 
 class Local implements FilesystemIntegration
 {
     public function isEnabled()
     {
-        return false;
+        return true;
     }
 
     public function upload($client_folder, $filename, $file): array
     {
-        return [];
+        Storage::disk('local')->put($client_folder, $file);
+        
+        return ['file_path' => $client_folder . '/' . $file->hashName()];
     }
 
     public function delete($full_path): bool
     {
-        return true;
+        return Storage::disk('local')->delete($full_path);
     }
 
     public function view($file)
     {
-        // TODO: Implement view() method.
+        return $this->download($file);
     }
 
     public function download($file)
     {
-        // TODO: Implement download() method.
+        $fileData =  Storage::disk('local')->get($file->path);
+        return $fileData;
     }
 
     public function revokeAccess()
