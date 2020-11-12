aa1ba372570ff1d90a9afb4f1f38f0821288bffe
Max Bosche <max@enten.space>
2020-11-12 20:51:59 +0100

    translation

--- app/Http/Middleware/Translation.php
+++ app/Http/Middleware/Translation.php
@@ -17,7 +17,7 @@ class Translation
     {
         if (auth()->user()) {
             $language = auth()->user()->language;
-            if (!in_array($language, ["en", "dk", "es"])) {
+            if (!in_array(strtolower($language), ["en", "de", "dk", "es"])) {
                 $language = "en";
             }
             app()->setLocale($language);
