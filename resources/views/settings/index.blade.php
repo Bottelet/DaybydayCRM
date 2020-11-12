8ef432dff6126f2e23986bba6874dfa13cbd375a
Max Bosche <max@enten.space>
2020-11-12 21:11:45 +0100

    added form options

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -58,9 +58,10 @@
                             <p class="small">@lang('This is the default language for new users, the language can be changed for each user under their profile')</p>
                             <br>
                             <select class="form-control" name="language">
-                                <option value="EN">@lang("English")</option>
-                                <option value="DK" {{$settings->language == "DK" ? "selected" : ""}}>@lang("Danish")</option>
-                                <option value="ES" {{$settings->language == "ES" ? "selected" : ""}}>@lang("Spanish")</option>
+                                <option value="en" {{strtolower($settings->language) == "en" ? "selected" : ""}}>@lang("English")</option>
+                                <option value="de" {{strtolower($settings->language) == "de" ? "selected" : ""}}>@lang("German")</option>
+                                <option value="dk" {{strtolower($settings->language) == "dk" ? "selected" : ""}}>@lang("Danish")</option>
+                                <option value="es" {{strtolower($settings->language) == "es" ? "selected" : ""}}>@lang("Spanish")</option>
                             </select>
                         </div>
                     </div>
