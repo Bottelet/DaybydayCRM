0d595083cf218b2d0bc9ea1ba0b046f01621f077
jordhanfelix <jordhanfelix@hotmail.com>
2020-12-02 22:19:43 -0300

    Adding portuguese brazil (português brasileiro) translation

---
+++ resources/lang/pt-br/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Essas credenciais não correspondem com os nossos registros.',
+    'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+];
