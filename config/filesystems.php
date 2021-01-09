b099fe8c6b99e3b1a22e4fae94797a8db7315028
Rm Yakovenko <rm.yakovenko@gmail.com>
2021-01-09 10:53:07 +0200

    Filesystem pubic disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,13 @@ return [
             'root'   => storage_path('app'),
         ],
 
+        'public' => [
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+        ],
+
         'ftp' => [
             'driver'   => 'ftp',
             'host'     => 'ftp.example.com',
