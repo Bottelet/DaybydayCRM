fade1e12fe50f3c189c3efc5913ca9e1c78b1202
Casper Bottelet <cbottelet@gmail.com>
2021-01-10 13:27:45 +0100

    if demo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,6 +26,7 @@
     </script>
     <!-- Global site tag (gtag.js) - Google Analytics Demo-->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-5NY2CK1XM9"></script>
+    <?php if(!isDemo()) { ?>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
@@ -33,6 +34,7 @@
 
     gtag('config', 'G-5NY2CK1XM9');
     </script>
+    <?php } ?>
     <script src="https://js.stripe.com/v3/"></script>
     @stack('style')
 </head>
