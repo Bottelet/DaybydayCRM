cc8716d284895710bc305b6a51c7aa81996a46a3
Casper Bottelet <cbottelet@gmail.com>
2021-04-17 10:05:54 +0200

    add missing all leads link on dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -115,6 +115,8 @@
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
+                    <a href="{{route('leads.index')}}" class="small-box-footer">{{ __('All Leads') }} <i
+                                class="fa fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
