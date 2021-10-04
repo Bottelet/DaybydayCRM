09b0b68c0901a8d0246ed797ca8005d7baee61a0
alamriku <alamriku069@gmail.com>
2021-10-04 22:01:39 +0600

    entrust added on lead create button at lead list

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.master')
 @section('heading')
     {{__('All Leads')}}
+    @if(Entrust::can('lead-create'))
     <a href="{{ route('leads.create')}}" class="btn btn-brand float-right">{{ __('New Lead') }}</a>
+    @endif
 @stop
 
 @section('content')
