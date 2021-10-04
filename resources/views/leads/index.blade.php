4a84f4ead7c2c47d07741e5eb8ea32a7e7de393a
alamriku <alamriku069@gmail.com>
2021-10-04 21:51:57 +0600

    new lead  button on lead list

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 @section('heading')
     {{__('All Leads')}}
+    <a href="{{ route('leads.create')}}" class="btn btn-brand float-right">{{ __('New Lead') }}</a>
 @stop
 
 @section('content')
