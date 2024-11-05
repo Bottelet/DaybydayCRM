1adfb1eca1d75147b881d76ec7808142870978e1
Nathan Smith <nathan@nathanwritescode.com>
2024-11-05 10:25:11 +0000

    Remove Cashier::ignoreMigrations() call from AppServiceProvider
    The Cashier::ignoreMigrations() method has been removed from the boot method in AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Cashier::ignoreMigrations();
         Client::observe(ClientObserver::class);
         Task::observe(TaskObserver::class);
         Lead::observe(LeadObserver::class);
