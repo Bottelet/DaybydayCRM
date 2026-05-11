e4a9afbb40bc3e4f98a9ae933e472b501d9be2f8
Niels Drost <nielsdrost7@gmail.com>
2026-05-11 09:50:04 +0200

    upgrade to vite

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,31 +1,11 @@
-const mix = require('laravel-mix');
-const webpack = require('webpack');
-
-mix.js('resources/assets/js/app.js', 'public/js').vue()
-    .js('resources/assets/js/jquery-init.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/','public/fonts/bootstrap')
-    .sass('resources/assets/sass/vendor.scss', './public/css/vendor.css')
-    .extract(['bootstrap-sass', 'jquery'])
-    .copy('node_modules/bootstrap-select/dist/css/bootstrap-select.min.css', 'public/css/bootstrap-select.min.css');
-
-
-mix.webpackConfig({
-    devServer: {
-        proxy: {
-            '*': 'http://localhost:80'
-        }
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-        })
-    ]
-});
-
-// version does not work in hmr mode
-if (process.env.npm_lifecycle_event !== 'hot') {
-    mix.version()
-}
+/**
+ * MIGRATED TO VITE
+ *
+ * This file is no longer used. The project has been migrated from Laravel Mix to Vite.
+ * See vite.config.js for the new build configuration.
+ *
+ * To build assets:
+ * - Development: yarn dev
+ * - Production: yarn build
+ * - Preview: yarn preview
+ */
