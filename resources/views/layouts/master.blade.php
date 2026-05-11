b95b1545c102f6bc697d36aa49f457540113390c
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2026-05-11 12:49:12 +0200

    Revert "upgrade to vite"
    This reverts commit c2ec63635c07967588634c91485fd2f74141a4f3.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,9 +11,11 @@
     <link href="{{ URL::asset('css/bootstrap-tour-standalone.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ URL::asset('css/picker.classic.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://unpkg.com/vis-timeline@7.3.4/styles/vis-timeline-graph2d.min.css">
+    <link rel="stylesheet" href="{{ mix('css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link href="https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    @vite(['resources/assets/sass/vendor.scss', 'resources/assets/sass/app.scss'])
+    <link rel="stylesheet" href="{{ mix('css/bootstrap-select.min.css') }}">
     <link href="{{ URL::asset('css/summernote.css') }}" rel="stylesheet">
     <link rel="shortcut icon" href="{{{ asset('images/favicon.png') }}}">
     <script>
@@ -118,10 +120,10 @@
                 class="fa fa-dollar sidebar-icon"></i><span id="menu-txt">{{ __('Sales') }}</span>
                 <i class="icon ion-md-arrow-dropup arrow-side sidebar-arrow"></i></a>
             <div class="collapse {{Request::is('invoices*') || Request::is('products*') ? 'in' : ''}}" id="sales">
-            <a href="{{ route('invoices.overdue')}}" class="list-group-item childlist">
+            <a href="{{ route('invoices.overdue')}}" class="list-group-item childlist"> 
                 <i class="bullet-point"><span></span></i> {{ __('Overdue') }}
             </a>
-            <a href="{{ route('products.index')}}" class="list-group-item childlist">
+            <a href="{{ route('products.index')}}" class="list-group-item childlist"> 
                 <i class="bullet-point"><span></span></i> {{ __('Products') }}
             </a>
             </div>
@@ -204,12 +206,15 @@
 
     <!-- /#page-content-wrapper -->
 </div>
-@vite(['resources/assets/js/app.js', 'resources/assets/js/jquery-init.js'])
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/jquery-init.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 <script>
     // Ensure jQuery and $ are globally available for scripts loaded via URL::asset()
-    if (typeof window.jQuery === 'undefined') {
+    if (typeof window.jQuery === 'undefined' && typeof __webpack_require__ !== 'undefined') {
         try {
-            window.jQuery = window.$ = require('jquery');
+            window.jQuery = window.$ = __webpack_require__(require.resolveWeak('jquery'));
         } catch (e) {}
     }
     window.jQuery = window.$ = window.jQuery || window.$;
